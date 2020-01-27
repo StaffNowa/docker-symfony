4aad6efec7223a374f87a7bd4fcec5c9a406d067
Vasilij Dusko | CREATION <vasilij@creation.lt>
2020-01-27 13:56:58 +0200

    * mysql_dump.sh - changed for database copy structure /tmp/db/backups/__YEAR__/__MONTH__/__DAY__

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 CHANGELOG for "Docker for Symfony"
 ======================
 
-* 1.0.0 (2020-01-26)
+* 1.0.6 (2020-01-26)
     * FEATURE   [PHP] Added ability to use imagecreatefromjpeg
     * FEATURE   [PHP] Added ability to change PHP version using command ./docker set php_version 5.6 / 7.0 / 7.2 / 7.3 / 7.4
     * FEATURE   [PHP] Added ability enable / disable xdebug using command ./docker set xdebug yes / no
--- scripts/mysql_dump.sh
+++ scripts/mysql_dump.sh
@@ -5,4 +5,14 @@
 # file name: backup+db_name+date.sql.gz
 #
 
-mysql -Ne "show databases" | grep -v "schema\|mysql\|information_schema" | while read db; do mysqldump $db | gzip > "/tmp/db/backup_"$db"_$(date +%Y%m%d).sql.gz"; done
+DAY=`date +%d`
+MONTH=`date +%m`
+YEAR=`date +%Y`
+
+BACKUP_DIR="/tmp/db/backups/${YEAR}/${MONTH}/${DAY}"
+
+if [ ! -d "${BACKUP_DIR}" ]; then
+    mkdir -p ${BACKUP_DIR}
+fi
+
+mysql -Ne "show databases" | grep -v "schema\|mysql\|information_schema" | while read db; do mysqldump $db | gzip > "${BACKUP_DIR}/backup_"$db"_$(date +%Y%m%d).sql.gz"; done
