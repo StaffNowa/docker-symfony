ff93072623fd2cf36d379247e4e21aa1ca576a44
Vasilij Duško <vasilij@prado.lt>
2020-02-25 20:28:46 +0200

    * mysql_dump.sh

--- scripts/mysql_dump.sh
+++ scripts/mysql_dump.sh
@@ -15,4 +15,4 @@ if [ ! -d "${BACKUP_DIR}" ]; then
     mkdir -p ${BACKUP_DIR}
 fi
 
-mysql -Ne "show databases" | grep -v "schema\|mysql\|information_schema" | while read db; do mysqldump $db | gzip > "${BACKUP_DIR}/backup_"$db"_$(date +%Y%m%d).sql.gz"; done
+mysql -Ne "show databases" | grep -v "schema\|mysql\|information_schema" | while read db; do mysqldump $db | gzip > "${BACKUP_DIR}/backup_"$db"_$(date +%Y%m%d_%H%M%S).sql.gz"; done
