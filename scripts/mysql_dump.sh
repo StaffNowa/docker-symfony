139950417bb3948bbcc43e0113129851908e342b
Vasilij Dusko | CREATION <vasilij@creation.lt>
2021-09-27 16:40:30 +0300

    * mysql_dump.sh - allow non-root user

--- scripts/mysql_dump.sh
+++ scripts/mysql_dump.sh
@@ -9,10 +9,12 @@ DAY=`date +%d`
 MONTH=`date +%m`
 YEAR=`date +%Y`
 
-BACKUP_DIR="/tmp/db/backups/${YEAR}/${MONTH}/${DAY}"
+BACKUP_MAIN_DIR="/tmp/db/backups"
+BACKUP_DIR="/${BACKUP_MAIN_DIR}/${YEAR}/${MONTH}/${DAY}"
 
 if [ ! -d "${BACKUP_DIR}" ]; then
     mkdir -p ${BACKUP_DIR}
 fi
 
 mysql -Ne "show databases" | grep -v "schema\|mysql\|information_schema" | while read db; do mysqldump $db | gzip > "${BACKUP_DIR}/backup_"$db"_$(date +%Y%m%d_%H%M%S).sql.gz"; done
+chown 1000:1000 -R ${BACKUP_MAIN_DIR}
