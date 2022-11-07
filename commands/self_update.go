11de2b7890e9d9072b138a1a4a1c53a46caeb875
Vasilij Dusko <vasilij@prado.lt>
2022-11-07 13:43:01 +0200

    Fix architecture detect for Linux OS.

--- commands/self_update.go
+++ commands/self_update.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"docker-symfony/util"
+	"fmt"
 	"github.com/symfony-cli/console"
 	"github.com/symfony-cli/terminal"
 	"runtime"
@@ -13,13 +14,15 @@ var selfUpdateCmd = &console.Command{
 	Aliases:  []*console.Alias{{Name: "self-update"}},
 	Usage:    "",
 	Action: func(c *console.Context) error {
+		os := runtime.GOOS
+		arch := runtime.GOARCH
+
 		util.ExecCommand("clear")
 		terminal.Println("We will process upgrading Docker for Symfony to the latest version.")
 
 		var filename = ""
-
-		if runtime.GOOS != "darwin" {
-			filename = "d4d_linux_arm64.tar.gz"
+		if os != "darwin" {
+			filename = fmt.Sprintf("d4d_linux_%s.tar.gz", arch)
 		} else {
 			filename = "d4d_darwin_all.tar.gz"
 		}
